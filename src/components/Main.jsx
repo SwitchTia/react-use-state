ede6e3e779938167ab9a034204a87277f996dae0
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-29 23:58:25 +0100

    Feat: Box

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,6 +11,10 @@ export default function Main({ languages }) {
             <Button key={language.id} language={language} />
           ))}
         </div>
+        <div className="box">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias vel adipisci, quidem porro, eum, consequatur cumque maxime necessitatibus libero dolor nihil provident ad nobis nulla nam nostrum quis error molestiae?</p>
+
+        </div>
       </div>
     </main>
   );
